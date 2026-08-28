d18c23c40688174cb6fb1a6500abf2f1d6b23c31
HenryAxon <153237565+HenryAxon@users.noreply.github.com>
2026-08-28 14:06:10 -0600

    jsut some slight changes

--- data_structures.py
+++ data_structures.py
@@ -110,54 +110,54 @@ class refiner:
 
 
 
-            
 mesh1 = mesh(10,20, 2)
-nodes, elems = mesh1.gen_init_mesh()
 
 
+refiner = refiner(mesh1)
 
+refiner.refine_h()
+refiner.refine_p()
 
-# next need way to assing dofs on each element according the p order of the element,  refining p is easy now - simply p+=1!
-
-
-def h_refine(marked, mesh):
-    elements = mesh.elems
-
-
-
-
+mesh1.active_elements
+mesh1.active_nodes
 
 
 
 
 
 
-# manually creating a small mesh to larn how the classes work together for 1D
-n0 = node(0, 0)
-n1 = node(1,1)
 
-n2 = node(2,2)
-n3 = node(3,3)
-n4= node(4,4)
 
-e1 = [n0,n1]
-e2 = [n1,n2]
-e3 = [n2, n3]
-e4= [n3, n4]
 
-# creating each element
-E1 = element(0, e1, p_order=2,level=0,parent=None)
-E2 = element(1, e2, p_order=2, level=0,parent=None)
-E3 = element(2, e3, p_order = 2, level=0, parent=None)
-E4 = element(3, e4, p_order=2, level=0, parent=None)
 
-# manual h-refinement:
-n5 = node(5,1.5,level=1)
-e5 = [n1,n5]
-e6 = [n5,n2]
-E5  = element(4,e5, p_order=2, level=1,parent =E2)
-E6 = element(5, e6,p_order=2, level=1,parent=E2)
 
-E2.children = [E5,E6]
-E2.active= False
+
+# # manually creating a small mesh to larn how the classes work together for 1D
+# n0 = node(0, 0)
+# n1 = node(1,1)
+
+# n2 = node(2,2)
+# n3 = node(3,3)
+# n4= node(4,4)
+
+# e1 = [n0,n1]
+# e2 = [n1,n2]
+# e3 = [n2, n3]
+# e4= [n3, n4]
+
+# # creating each element
+# E1 = element(0, e1, p_order=2,level=0,parent=None)
+# E2 = element(1, e2, p_order=2, level=0,parent=None)
+# E3 = element(2, e3, p_order = 2, level=0, parent=None)
+# E4 = element(3, e4, p_order=2, level=0, parent=None)
+
+# # manual h-refinement:
+# n5 = node(5,1.5,level=1)
+# e5 = [n1,n5]
+# e6 = [n5,n2]
+# E5  = element(4,e5, p_order=2, level=1,parent =E2)
+# E6 = element(5, e6,p_order=2, level=1,parent=E2)
+
+# E2.children = [E5,E6]
+# E2.active= False
 
